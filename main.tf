18e1d8fe747f148a3d8f807c3136872cbb4d049c
kamal <kamalguntur2000@gmail.com>
2025-11-28 18:05:16 +0530

    main

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Security Group for EC2
+resource "aws_security_group" "ec2_sg" {
+  name        = "terraform-demo-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "all-key"                       # <-- your AWS key name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "terraform-jenkins-ec2"
+  }
+}
+
---
+++ output.tf
@@ -0,0 +1,13 @@
+
+output "instance_id" {
+  value = aws_instance.server.id
+}
+
+output "public_ip" {
+  value = aws_instance.server.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i all-key.pem ec2-user@${aws_instance.server.public_ip}"
+}
+
